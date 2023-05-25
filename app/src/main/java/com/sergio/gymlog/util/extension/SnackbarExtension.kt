0246dc4387b48fbf92c0972d3141afca2a5f5be0
Sergio Mateo Moreno <sergiomm50@gmail.com>
2023-05-25 15:23:41 +0200

    Pequeña moficación

--- app/src/main/java/com/sergio/gymlog/util/extension/SnackbarExtension.kt
+++ app/src/main/java/com/sergio/gymlog/util/extension/SnackbarExtension.kt
@@ -6,8 +6,8 @@ import com.google.android.material.snackbar.Snackbar
 import com.sergio.gymlog.R
 
 fun View.createTopSnackBar(parent : View, messageResource : Int, duration: Int = Snackbar.LENGTH_SHORT) {
-    val snackBar = Snackbar.make(context, parent, context.getString(messageResource), duration)
-    snackBar.setAction(context.getString(R.string.accept)) {
+    val snackBar = Snackbar.make( parent, messageResource, duration)
+    snackBar.setAction(R.string.accept) {
         snackBar.dismiss()     }
 
     val params = snackBar.view.layoutParams as FrameLayout.LayoutParams
